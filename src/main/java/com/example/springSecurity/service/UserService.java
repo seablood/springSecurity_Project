b259728a416f923f2a69d0ba381c58c3599dbde9
seablood <dudwnszero99@gmail.com>
2024-11-30 16:30:10 +0900

    push error

--- src/main/java/com/example/springSecurity/service/UserService.java
+++ src/main/java/com/example/springSecurity/service/UserService.java
@@ -5,6 +5,7 @@ import com.example.springSecurity.model.User;
 import com.example.springSecurity.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -13,11 +14,14 @@ import org.springframework.stereotype.Service;
 public class UserService {
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Value("${jwt.secretKey}")
+    private String secretKey;
 
     @Transactional
     public User save(CreateUserDTO dto){
         dto.setPassword(bCryptPasswordEncoder.encode(dto.getPassword()));
         User user = CreateUserDTO.toEntity(dto);
+        System.out.println(secretKey);
         return userRepository.save(user);
     }
 }
